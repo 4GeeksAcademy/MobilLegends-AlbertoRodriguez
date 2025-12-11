872323ba80f3dc6fd22f54000867503cc0dccbd2
Contreras486 <contreras65.jc@gmail.com>
2025-12-11 19:32:58 +0000

    Falta terminar

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,22 +1,33 @@
 export const Card = (props) => {
 
-    return(
-       <div className="col-sm-6 col-md-4 p-2">
-        <div className="card">
-            <div className=" d-flex justify-content-center" style={{width: "100%",overflow: "hidden", aspectRatio: "3/4"}}>
-                <img src={props.personaje.imagen} alt="..." />
-            </div>
-            <div className="card-body">
-                <h4 className="card-title">{props.personaje.nombre}</h4>
-                <p className="card-text"><strong>Linea: </strong> {props.personaje.linea}</p>
+    return (
+        <div className="col-sm-6 col-md-4 p-2">
+            <div className="card">
+                <div className=" d-flex justify-content-center" style={{ width: "100%", overflow: "hidden", aspectRatio: "3/4" }}>
+                    <img src={props.personaje.imagen} alt="..." />
+                </div>
+                <div className="card-body">
+                    <h4 className="card-title">{props.personaje.nombre}</h4>
+                    <p className="card-text"><strong>Linea: </strong> {props.personaje.linea}</p>
+                    <div className="abilidades-container">
+                        <h5>Habilidades</h5>
+                        <ul>
+                            {props.personaje.abilidades.map((habilidad,index) =>{
+                                return(
+                                    <li key={index}>{habilidad}</li>
+                                )
+                            })}
+                        </ul>
+
+                    </div>
+
+                </div>
 
             </div>
 
         </div>
 
-       </div>
 
-        
     )
 }
 
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -14,63 +14,63 @@ const Home = () => {
 			id: 1,
 			nombre: "Karrie",
 			linea: "Tirador",
-			abilidades: [" Marca de Rueda de luz, Daño verdadero, Velocidad de ataque"],
+			abilidades: [" Marca de Rueda de luz"," Daño verdadero", "Velocidad de ataque"],
 			imagen: "https://i.pinimg.com/736x/c1/be/6a/c1be6ad79df0ea5aed779b0587d843f5.jpg"
 		},
 		{
 			id: 2,
 			nombre: "Bruno",
 			linea: "Tirador",
-			abilidades: ["Piernas mecanicas, Tiro bolea, barrida, Balonazo"],
+			abilidades: ["Piernas mecanicas", "Tiro bolea", "barrida"],
 			imagen: "https://i.pinimg.com/564x/b6/20/95/b62095275b353309b443619153164b43.jpg"
 		},
 		{
 			id: 3,
 			nombre: "Grock",
 			linea: "Tanque",
-			abilidades: ["Control de masas, Poder de la naturaleza,Barrera de Guardian,Embestida"],
+			abilidades: ["Control de masas", "Poder de la naturaleza","Barrera de Guardian"],
 			imagen: "https://theriagames.com/wp-content/uploads/2025/03/Mobile-Legends-Grock-3.webp"
 		},
 		{
 			id: 4,
 			nombre: "Zilong",
 			linea: "Asesino",
-			abilidades: ["Golpe de lanza,Gerrero supremo,Rafaga "],
+			abilidades: ["Golpe de lanza","Gerrero supremo","Rafaga "],
 			imagen: "https://pm1.aminoapps.com/6606/d4573e455148d4ac3668c2dc7c70b5f5c3886a70_hq.jpg"
 		},
 		{
 			id: 5,
 			nombre: "Saber",
 			linea: "Asesino",
-			abilidades: ["Se especializa en enboscadas"],
+			abilidades: ["Se especializa en enboscadas","Asesinato al instante","Golpe final"],
 			imagen: "https://pm1.aminoapps.com/6704/09eea52919644dba2b1c8cab63910ef6473f8a39_00.jpg"
 		},
 		{
 			id: 6,
 			nombre: "Vale",
 			linea: "Mago",
-			abilidades: ["Alas de viento, Tornado,Tempestad suprema"],
+			abilidades: ["Alas de viento", "Tornado","Tempestad suprema"],
 			imagen: "https://cdn.oneesports.gg/cdn-data/2022/07/MLBB_Vale-1.jpg"
 		},
 		{
 			id: 7,
 			nombre: "Ciclope",
 			linea: "Mago",
-			abilidades: ["Impacto Estelar ,Ataque Planetario, Bloqueo de Energía Estelar"],
+			abilidades: ["Impacto Estelar" ,"Ataque Planetario", "Bloqueo de Energía Estelar"],
 			imagen: "https://pm1.aminoapps.com/7259/4eb4de2dab26a156f9a22bd89abd1801d803a290r1-607-1080v2_hq.jpg"
 		},
 		{
 			id: 8,
 			nombre: "Rafaela",
 			linea: "suport",
-			abilidades: ["Luz de Reprensión,Curación Santa,Rayo Sagrado"],
+			abilidades: ["Luz de Reprensión","Curación Santa","Rayo Sagrado"],
 			imagen: "https://theriagames.com/wp-content/uploads/2025/03/Mobile-Legends-Rafaela.webp"
 		},
 		{
 			id: 9,
 			nombre: "Estes",
 			linea: "suport",
-			abilidades: ["Inmersión de la Luz de la Luna, Dominio de la Diosa de la Luna,Bendición de la Diosa de la Luna"],
+			abilidades: ["Inmersión de la Luz de la Luna"," Dominio de la Diosa de la Luna","Bendición de la Diosa de la Luna"],
 			imagen: "https://liquipedia.net/commons/images/0/0a/Estes_infobox.png"
 		}
 
